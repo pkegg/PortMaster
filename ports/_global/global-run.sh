dbb42cd1039d8f51b51a3b88b68f6f46ddff3520
pkegg <pkegg@users.noreply.github.com>
2021-11-01 00:54:29 -0600

    Switch run.sh to run

--- ports/_global/global-run.sh
+++ ports/_global/global-run.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 set -e
 DIR="$(realpath $( dirname "${BASH_SOURCE[0]}" ))"
-PACKAGE="$1"
-if [[ -z "$PACKAGE" ]]; then
-  # This should be replaced on install
-  PACKAGE=__PACKAGE__
-fi
+
+# This should be replaced on install
+PACKAGE=__PACKAGE__
+
 pushd "${DIR}/${PACKAGE}" &> /dev/null
 bash ./run.sh
